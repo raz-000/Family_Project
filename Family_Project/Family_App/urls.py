e1224966176c700c1ed802aa2dd3652b55f79716
Ricardo <ricardo.almario@gmail.com>
2022-04-03 17:35:34 -0500

    Yous can ingress family person in db

--- Family_Project/Family_App/urls.py
+++ Family_Project/Family_App/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.family, name='family'),
     path('index.html', views.family, name='family'),
     path('generic.html', views.family_ingress, name='family_ingress'),
-
+    path('formulario/', views.formulario, name='formulario'),
 ]
 
--- Family_Project/Family_App/views.py
+++ Family_Project/Family_App/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse # Necesario para Generar respuestas sobre http
+from .models import *
+from .forms import *
 # Create your views here.
 
 # def family(request):
@@ -10,3 +12,36 @@ def family(request):
 
 def family_ingress(request):
     return render(request, "index.html")
+
+def formulario(request):
+#    if request.method == 'POST':
+    print('----------------------------------------')
+    print(request.POST)
+
+    nombre = request.POST['nombre']
+    profesion = request.POST['profesion']
+    edad = request.POST['edad']
+    email = request.POST['email']
+    descripcion = request.POST['descripcion']
+
+    nuevo_inegrante = Integrante_Familia(nombre=nombre, profesion=profesion, edad=edad, email=email, descripcion=descripcion)
+    nuevo_inegrante.save()
+
+    return render(request, "index3_formulario.html", {'nombre': nombre})
+
+        # miFormulario = Formularios_Family(request.POST)
+        # print(miFormulario)
+
+        # if miFormulario.is_valid:
+        #     informacion = miFormulario.cleaned_data
+        #     familia = Integrante_Familia(nombre=informacion['nombre'], profesion=informacion['profesion'], edad=informacion['edad'], email=informacion['email'], descripcion=informacion['descripcion'])
+        #     familia.save()
+        #     return render(request, "index3_formulario.html")
+
+
+# #       return HttpResponse(request.POST.items())
+#         formulario = Integrante_Familia(request.POST['nombre'], request.POST['profesion'], request.POST['edad'], request.POST['email'], request.POST['descripcion'])
+#         # return f'Esta funcionando {nombre}'
+#         formulario.save()
+#         return render(request, "index3_formulario.html")
+#
